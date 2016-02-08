42fcb630c5301193211d6d7f73bb0aaad9d680b0
Florian Schmaus <flo@geekplace.eu>
2016-02-08 17:44:47 +0100

    Signal a failed integration test run
    and provide some stats about the integration test run.

--- minidns-integration-test/src/main/java/de/measite/minidns/integrationtest/IntegrationTestHelper.java
+++ minidns-integration-test/src/main/java/de/measite/minidns/integrationtest/IntegrationTestHelper.java
@@ -12,33 +12,63 @@ package de.measite.minidns.integrationtest;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class IntegrationTestHelper {
-    private static Set<Class<?>> testClasses;
+    private static Set<Class<?>> testClasses = new HashSet<>();
     private static Logger LOGGER = Logger.getLogger(IntegrationTestHelper.class.getName());
 
+    enum TestResult {
+        Success,
+        Failure,
+        ;
+    }
+
     static {
-        testClasses = new HashSet<>();
         testClasses.add(CoreTest.class);
         testClasses.add(DNSSECTest.class);
         testClasses.add(DaneTest.class);
     }
 
     public static void main(String[] args) {
+        int testsRun = 0;
+        List<Method> successfulTests = new ArrayList<>();
+        List<Method> failedTests = new ArrayList<>();
         for (final Class<?> aClass : testClasses) {
             for (final Method method : aClass.getDeclaredMethods()) {
                 if (method.isAnnotationPresent(IntegrationTest.class)) {
-                    invokeTest(method, aClass);
+                    TestResult result = invokeTest(method, aClass);
+                    testsRun++;
+                    switch (result) {
+                    case Success:
+                        successfulTests.add(method);
+                        break;
+                    case Failure:
+                        failedTests.add(method);
+                        break;
+                    }
                 }
             }
         }
+        StringBuilder resultMessage = new StringBuilder();
+        resultMessage.append("MiniDNS Integration Test Result: [").append(successfulTests.size()).append('/').append(testsRun).append("] ");
+        int exitStatus = 0;
+        if (failedTests.isEmpty()) {
+            resultMessage.append("SUCCESS \\o/");
+        } else {
+            resultMessage.append("FAILURE :(");
+            exitStatus = 2;
+        }
+        LOGGER.info(resultMessage.toString());
+        System.exit(exitStatus);
     }
 
-    public static void invokeTest(Method method, Class<?> aClass) {
+    public static TestResult invokeTest(Method method, Class<?> aClass) {
         Class<?> expected = method.getAnnotation(IntegrationTest.class).expected();
         if (!Exception.class.isAssignableFrom(expected)) expected = null;
 
@@ -47,17 +77,22 @@ public class IntegrationTestHelper {
 
             if (expected != null) {
                 LOGGER.logp(Level.WARNING, aClass.getName(), method.getName(), "Test failed: expected exception " + expected + " was not thrown!");
+                return TestResult.Failure;
             } else {
                 LOGGER.logp(Level.INFO, aClass.getName(), method.getName(), "Test suceeded.");
+                return TestResult.Success;
             }
         } catch (InvocationTargetException e) {
             if (expected != null && expected.isAssignableFrom(e.getTargetException().getClass())) {
                 LOGGER.logp(Level.INFO, aClass.getName(), method.getName(), "Test suceeded.");
+                return TestResult.Success;
             } else {
                 LOGGER.logp(Level.WARNING, aClass.getName(), method.getName(), "Test failed: unexpected exception was thrown: ", e.getTargetException());
+                return TestResult.Failure;
             }
         } catch (IllegalAccessException | NullPointerException e) {
             LOGGER.logp(Level.SEVERE, aClass.getName(), method.getName(), "Test failed: could not invoke test, is it public static?");
+            return TestResult.Failure;
         }
     }
 }
