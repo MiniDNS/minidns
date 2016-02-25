9bba61c0313871c3ca8ae5e00efabb7f9987ee89
Florian Schmaus <flo@geekplace.eu>
2016-02-25 17:40:30 +0100

    Add 'traceMinidns' feature to REPL

--- minidns-repl/src/main/java/de/measite/minidns/minidnsrepl/MiniDnsRepl.java
+++ minidns-repl/src/main/java/de/measite/minidns/minidnsrepl/MiniDnsRepl.java
@@ -10,7 +10,19 @@
  */
 package de.measite.minidns.minidnsrepl;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Field;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import de.measite.minidns.AbstractDNSClient;
 import de.measite.minidns.DNSClient;
@@ -35,5 +47,35 @@ public class MiniDnsRepl {
         defaultCacheField.setAccessible(true);
         LRUCache defaultCache = (LRUCache) defaultCacheField.get(null);
         defaultCache.clear();
+    }
+
+    private static final Logger MINIDNS_LOGGER = Logger.getLogger("de.measite.minidns");
+    private static final InputStream LOG_MANAGER_CONFIG = new ByteArrayInputStream((
+// @formatter:off
+"de.measite.minidns.level=FINEST" + '\n'
+).getBytes()
+);
+// @formatter:on
+    private static final SimpleDateFormat LOG_TIME_FORMAT = new SimpleDateFormat("hh:mm:ss.SSS");
+
+    public static void traceMinidns() throws SecurityException, IOException {
+        LogManager.getLogManager().readConfiguration(LOG_MANAGER_CONFIG);
+        Handler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.FINEST);
+        consoleHandler.setFormatter(new Formatter() {
+            @Override
+            public String format(LogRecord logRecord) {
+                StringBuffer sb = new StringBuffer(256);
+                Date date = new Date(logRecord.getMillis());
+                String dateString;
+                synchronized (LOG_TIME_FORMAT) {
+                    dateString = LOG_TIME_FORMAT.format(date);
+                }
+                sb.append(dateString).append(' ').append(logRecord.getLoggerName()).append(' ').append(logRecord.getSourceMethodName()).append('\n');
+                sb.append(logRecord.getLevel()).append(' ').append(logRecord.getMessage()).append('\n');
+                return sb.toString();
+            }
+        });
+        MINIDNS_LOGGER.addHandler(consoleHandler);
     }
 }
