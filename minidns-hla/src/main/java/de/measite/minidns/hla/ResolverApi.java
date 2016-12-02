0227c356d28ef83ff48a238572cce9c98cfdbd27
Florian Schmaus <flo@geekplace.eu>
2016-12-02 10:56:33 +0100

    Add ResovlerApi.resolveDnssec()

--- minidns-hla/src/main/java/de/measite/minidns/hla/ResolverApi.java
+++ minidns-hla/src/main/java/de/measite/minidns/hla/ResolverApi.java
@@ -18,13 +18,34 @@ import de.measite.minidns.DNSMessage;
 import de.measite.minidns.DNSName;
 import de.measite.minidns.Question;
 import de.measite.minidns.Record.TYPE;
+import de.measite.minidns.cache.LRUCache;
 import de.measite.minidns.dnssec.DNSSECClient;
 import de.measite.minidns.dnssec.DNSSECMessage;
 import de.measite.minidns.dnssec.UnverifiedReason;
 import de.measite.minidns.record.Data;
 import de.measite.minidns.recursive.ReliableDNSClient;
+import de.measite.minidns.recursive.ReliableDNSClient.Mode;
 
 public class ResolverApi {
+
+    public static final ResolverApi ITERATIVE_ONLY_DNSSEC;
+    public static final ResolverApi RECURSIVE_ONLY_DNSSEC;
+
+    static {
+        // Set the *_ONLY_DNSSEC ResolverApi. It is important that the two do *not* share the same cache, since we
+        // probably fall back to iterativeOnly and in that case do not want the cached results of the recursive result.
+        {
+            DNSSECClient iterativeOnlyDnssecClient = new DNSSECClient(new LRUCache(1024));
+            iterativeOnlyDnssecClient.setMode(Mode.iterativeOnly);
+            ITERATIVE_ONLY_DNSSEC = new ResolverApi(iterativeOnlyDnssecClient);
+        }
+
+        {
+            DNSSECClient recursiveOnlyDnssecClient = new DNSSECClient(new LRUCache(1024));
+            recursiveOnlyDnssecClient.setMode(Mode.recursiveOnly);
+            RECURSIVE_ONLY_DNSSEC = new ResolverApi(recursiveOnlyDnssecClient);
+        }
+    }
 
     public static final ResolverApi DNSSEC = new ResolverApi(new DNSSECClient());
     public static final ResolverApi NON_DNSSEC = new ResolverApi(new ReliableDNSClient());
@@ -64,6 +85,53 @@ public class ResolverApi {
         }
 
         return new ResolverResult<D>(question, dnsMessage, unverifiedReasons);
+    }
+
+    /**
+     * Resolve the given name and type which is expected to yield DNSSEC authenticated results.
+     *
+     * @param name the DNS name to resolve.
+     * @param type the class of the RR type to resolve.
+     * @param <D> the RR type to resolve.
+     * @return the resolver result.
+     * @throws IOException in case an exception happens while resolving.
+     * @see #resolveDnssec(Question)
+     */
+    public static <D extends Data> ResolverResult<D> resolveDnssec(String name, Class<D> type) throws IOException {
+        return resolveDnssec(DNSName.from(name), type);
+    }
+
+    /**
+     * Resolve the given name and type which is expected to yield DNSSEC authenticated results.
+     *
+     * @param name the DNS name to resolve.
+     * @param type the class of the RR type to resolve.
+     * @param <D> the RR type to resolve.
+     * @return the resolver result.
+     * @throws IOException in case an exception happens while resolving.
+     * @see #resolveDnssec(Question)
+     */
+    public static <D extends Data> ResolverResult<D> resolveDnssec(DNSName name, Class<D> type) throws IOException {
+        TYPE t = TYPE.getType(type);
+        Question q = new Question(name, t);
+        return resolveDnssec(q);
+    }
+
+    /**
+     * Resolve the given question which is expected to yield DNSSEC authenticated results.
+     *
+     * @param question the question to resolve.
+     * @param <D> the RR type to resolve.
+     * @return the resolver result.
+     * @throws IOException in case an exception happens while resolving.
+     */
+    public static <D extends Data> ResolverResult<D> resolveDnssec(Question question) throws IOException {
+        ResolverResult<D> res = RECURSIVE_ONLY_DNSSEC.resolve(question);
+        if (res.isAuthenticData())
+            return res;
+
+        // Fall back to iterative resolution.
+        return ITERATIVE_ONLY_DNSSEC.resolve(question);
     }
 
     public AbstractDNSClient getClient() {
