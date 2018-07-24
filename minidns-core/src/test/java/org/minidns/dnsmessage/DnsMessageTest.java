fab8c31e00217751afb9a3105fb0c68f95f28238
Florian Schmaus <flo@geekplace.eu>
2018-07-24 12:17:12 +0200

    Add unit test for TXT with multiple character-strings

--- minidns-core/src/test/java/org/minidns/dnsmessage/DnsMessageTest.java
+++ minidns-core/src/test/java/org/minidns/dnsmessage/DnsMessageTest.java
@@ -167,6 +167,30 @@ public class DnsMessageTest {
         assertEquals(txtToBeFound.size(), 0);
     }
 
+    @Test
+    public void testTXTMultiCharacterStringLookup() throws IOException {
+        DnsMessage dnsMessage = getMessageFromResource("gmail-domainkey-txt");
+        assertEquals(1, dnsMessage.answerSection.size());
+
+        Record<?> answerRecord = dnsMessage.answerSection.get(0);
+
+        assertEquals(TYPE.TXT, answerRecord.type);
+        Record<TXT> txtRecord = answerRecord.as(TXT.class);
+
+        List<String> characterStrings = txtRecord.payloadData.getCharacterStrings();
+        assertEquals(2, characterStrings.size());
+        assertEquals(
+                "k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAviPGBk4ZB64UfSqWyAicdR7lodhytae+EYRQVtKDhM+1mXjEqRtP/pDT3sBhazkmA48n2k5NJUyMEoO8nc2r6sUA+/Dom5jRBZp6qDKJOwjJ5R/OpHamlRG+YRJQqR",
+                characterStrings.get(0));
+        assertEquals(
+                "tqEgSiJWG7h7efGYWmh4URhFM9k9+rmG/CwCgwx7Et+c8OMlngaLl04/bPmfpjdEyLWyNimk761CX6KymzYiRDNz1MOJOJ7OzFaS4PFbVLn0m5mf0HVNtBpPwWuCNvaFVflUYxEyblbB6h/oWOPGbzoSgtRA47SHV53SwZjIsVpbq4LxUW9IxAEwYzGcSgZ4n5Q8X8TndowsDUzoccPFGhdwIDAQAB",
+                characterStrings.get(1));
+
+        String text = txtRecord.payloadData.getText();
+        assertEquals(
+                "k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAviPGBk4ZB64UfSqWyAicdR7lodhytae+EYRQVtKDhM+1mXjEqRtP/pDT3sBhazkmA48n2k5NJUyMEoO8nc2r6sUA+/Dom5jRBZp6qDKJOwjJ5R/OpHamlRG+YRJQqR / tqEgSiJWG7h7efGYWmh4URhFM9k9+rmG/CwCgwx7Et+c8OMlngaLl04/bPmfpjdEyLWyNimk761CX6KymzYiRDNz1MOJOJ7OzFaS4PFbVLn0m5mf0HVNtBpPwWuCNvaFVflUYxEyblbB6h/oWOPGbzoSgtRA47SHV53SwZjIsVpbq4LxUW9IxAEwYzGcSgZ4n5Q8X8TndowsDUzoccPFGhdwIDAQAB",
+                text);
+    }
 
     @Test
     public void testSoaLookup() throws Exception {
