MiniDNS/minidns

commit 82c8a107bb9fc3c1d99db42494c29381042e653f

[dnssec] Fix ASN.1 encoding if DSA signatures
Starting with JDK 8u121 the Java became more strict about DER
encoding. The particular case we are hitting here are leading zeros in
ASN.1 DER integer encoding, which will be rejected in Java 8u121. See

https://bugs.openjdk.java.net/browse/JDK-8174719
and
http://hg.openjdk.java.net/jdk8u/jdk8u/jdk/rev/31dae74ad077#l5.16

Fixes

DsaSingatureVerifierTest > testDSAWrongSignature() FAILED
    org.minidns.dnssec.DnssecValidationFailedException: Validating signature failed
        at org.minidns.dnssec.algorithms.JavaSecSignatureVerifier.verify(JavaSecSignatureVerifier.java:55)
        at org.minidns.dnssec.algorithms.SignatureVerifierTest.verify(SignatureVerifierTest.java:54)
        at org.minidns.dnssec.algorithms.SignatureVerifierTest.assertSignatureInvalid(SignatureVerifierTest.java:46)
        at org.minidns.dnssec.algorithms.DsaSingatureVerifierTest.testDSAWrongSignature(DsaSingatureVerifierTest.java:56)

        Caused by:
        java.security.SignatureException: Invalid encoding for signature
            at java.base/sun.security.provider.DSA.engineVerify(DSA.java:346)
            at java.base/sun.security.provider.DSA.engineVerify(DSA.java:292)
            at java.base/java.security.Signature$Delegate.engineVerify(Signature.java:1416)
            at java.base/java.security.Signature.verify(Signature.java:790)
            at org.minidns.dnssec.algorithms.JavaSecSignatureVerifier.verify(JavaSecSignatureVerifier.java:50)
            ... 3 more

            Caused by:
            java.io.IOException: Invalid encoding: redundant leading 0s
                at java.base/sun.security.util.DerInputBuffer.getBigInteger(DerInputBuffer.java:161)
                at java.base/sun.security.util.DerValue.getBigInteger(DerValue.java:545)
                at java.base/sun.security.provider.DSA.engineVerify(DSA.java:344)
                ... 7 more

minidns-dnssec/src/main/java/org/minidns/dnssec/algorithms/DsaSignatureVerifier.java
@@ -43,28 +43,26 @@ class DsaSignatureVerifier extends JavaSecSignatureVerifier {
         @SuppressWarnings("unused")
         byte t = dis.readByte();
 
-        byte[] r = new byte[LENGTH];
-        dis.readFully(r);
-        int rlen = (r[0] < 0) ? LENGTH + 1 : LENGTH;
+        byte[] r = new byte[LENGTH + 1];
+        dis.readFully(r, 1, LENGTH);
+        int roff = 0;
+        while(roff < LENGTH && r[roff] == 0 && r[roff + 1] < 0) roff++;
 
-        byte[] s = new byte[LENGTH];
-        dis.readFully(s);
-        int slen = (s[0] < 0) ? LENGTH + 1 : LENGTH;
+        byte[] s = new byte[LENGTH + 1];
+        dis.readFully(s, 1, LENGTH);
+        int soff = 0;
+        while(soff < LENGTH && s[soff] == 0 && r[soff + 1] < 0) soff++;
 
         dos.writeByte(0x30);
-        dos.writeByte(rlen + slen + 4);
+        dos.writeByte(r.length - roff + s.length - soff + 4);
 
         dos.writeByte(0x2);
-        dos.writeByte(rlen);
-        if (rlen > LENGTH)
-            dos.writeByte(0);
-        dos.write(r);
+        dos.writeByte(r.length - roff);
+        dos.write(r, roff, r.length - roff);
 
         dos.writeByte(0x2);
-        dos.writeByte(slen);
-        if (slen > LENGTH)
-            dos.writeByte(0);
-        dos.write(s);
+        dos.writeByte(s.length - soff);
+        dos.write(s, soff, s.length - soff);
         } catch (IOException e) {
             throw new DataMalformedException(e, rrsig.getSignature());
         }

commit 99a0b7df83529801cdc606b7cdba358bee0b8b8b

[dnssec] Fix ASN.1 encoding if DSA signatures for realsies
Turns out the previous "fix" 82c8a107bb9f ("[dnssec] Fix ASN.1
encoding if DSA signatures") was just broken. Hopefully this does the trick.

Fixes: 82c8a107bb9f ("[dnssec] Fix ASN.1 encoding if DSA signatures")

minidns-dnssec/src/main/java/org/minidns/dnssec/algorithms/DsaSignatureVerifier.java
@@ -35,34 +35,60 @@ class DsaSignatureVerifier extends JavaSecSignatureVerifier {
     @Override
     protected byte[] getSignature(RRSIG rrsig) throws DataMalformedException {
         DataInput dis = rrsig.getSignatureAsDataInputStream();
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        DataOutputStream dos = new DataOutputStream(bos);
 
+        ByteArrayOutputStream bos;
         try {
-        // Convert RFC 2536 to ASN.1
-        @SuppressWarnings("unused")
-        byte t = dis.readByte();
+            // Convert RFC 2536 to ASN.1
+            @SuppressWarnings("unused")
+            byte t = dis.readByte();
 
-        byte[] r = new byte[LENGTH + 1];
-        dis.readFully(r, 1, LENGTH);
-        int roff = 0;
-        while (roff < LENGTH && r[roff] == 0 && r[roff + 1] < 0) roff++;
+            byte[] r = new byte[LENGTH];
+            dis.readFully(r);
+            int roff = 0;
+            final int rlen;
+            if (r[0] == 0) {
+                while (roff < LENGTH && r[roff] == 0) {
+                    roff++;
+                }
+                rlen = r.length - roff;
+            } else if (r[0] < 0) {
+                rlen = r.length + 1;
+            } else {
+                rlen = r.length;
+            }
 
-        byte[] s = new byte[LENGTH + 1];
-        dis.readFully(s, 1, LENGTH);
-        int soff = 0;
-        while (soff < LENGTH && s[soff] == 0 && r[soff + 1] < 0) soff++;
+            byte[] s = new byte[LENGTH];
+            dis.readFully(s);
+            int soff = 0;
+            final int slen;
+            if (s[0] == 0) {
+                while (soff < LENGTH && s[soff] == 0) {
+                    soff++;
+                }
+                slen = s.length - soff;
+            } else if (s[0] < 0) {
+                slen = s.length + 1;
+            } else {
+                slen = s.length;
+            }
 
-        dos.writeByte(0x30);
-        dos.writeByte(r.length - roff + s.length - soff + 4);
+            bos = new ByteArrayOutputStream();
+            DataOutputStream dos = new DataOutputStream(bos);
 
-        dos.writeByte(0x2);
-        dos.writeByte(r.length - roff);
-        dos.write(r, roff, r.length - roff);
+            dos.writeByte(0x30);
+            dos.writeByte(rlen + slen + 4);
 
-        dos.writeByte(0x2);
-        dos.writeByte(s.length - soff);
-        dos.write(s, soff, s.length - soff);
+            dos.writeByte(0x2);
+            dos.writeByte(rlen);
+            if (rlen > LENGTH)
+                dos.writeByte(0);
+            dos.write(r, roff, LENGTH - roff);
+
+            dos.writeByte(0x2);
+            dos.writeByte(slen);
+            if (slen > LENGTH)
+                dos.writeByte(0);
+            dos.write(s, soff, LENGTH - soff);
         } catch (IOException e) {
             throw new DataMalformedException(e, rrsig.getSignature());
         }